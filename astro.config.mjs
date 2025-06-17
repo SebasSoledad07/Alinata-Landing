63bcabfa1b621ef59a552be923d3c2358592a3a7
SebasSoledad07 <sebastiansg@ufps.edu.co>
2025-06-17 19:10:59 -0400

    chore: update dependencies and add new type definitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
   plugins: [tailwindcss()],
 },
-
-  integrations: [react()],
+output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
